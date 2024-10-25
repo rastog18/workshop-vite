f01b8c33f1ed2dce621dcc991c153ae41093202a
Shivam Rastogi <130913444+rastog18@users.noreply.github.com>
2024-10-25 19:42:00 -0400

    fixing errors

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { useParams } from "react-router-dom";
-<<<<<<< HEAD
 import DemoPage from "./pages/demoPage.jsx";
 import "./index.css";
 import Home from "./Home.jsx";
 import Example from "./pages/example.jsx";
 import PradyunTPage from "./pages/PradyunTPage.jsx";
-=======
 import DemoPage from './pages/demoPage.jsx'
 import RonaldPage from './pages/RonaldPage.jsx'
 import MatthewPage from './pages/MatthewPage'
@@ -17,7 +15,6 @@ import Mihika from "./pages/Mihika.jsx";
 import './index.css'
 import Home from './Home.jsx'
 import Example from './pages/example.jsx';
->>>>>>> b3d378d6ac212a448fcebda2b8e553a839a379ec
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -26,10 +23,7 @@ createRoot(document.getElementById("root")).render(
         <Route path="/" element={<Home />} />
         <Route path="/example" element={<Example />} />
         <Route path="/demo" element={<DemoPage />} />
-<<<<<<< HEAD
-=======
         <Route path="/mihika" element={<Mihika />} />
->>>>>>> b3d378d6ac212a448fcebda2b8e553a839a379ec
         <Route path="/profile/:name" element={<ProfileRouter />} />
       </Routes>
     </Router>
@@ -44,7 +38,6 @@ function ProfileRouter() {
       return <DemoPage />;
 
     // Change <DemoPage /> to your custom profile page, and change the case to your name
-<<<<<<< HEAD
     case "Person1":
       return <DemoPage />;
     case "Person2":
@@ -55,7 +48,6 @@ function ProfileRouter() {
       return <DemoPage />;
     case "Pradyun Tandra":
       return <PradyunTPage />;
-=======
     case "Shivam":
       return <ShivamPage />;
     case "Ronald Tsai":
@@ -66,17 +58,7 @@ function ProfileRouter() {
       return <Mihika />;
     case "Person5":
       return <DemoPage />;
->>>>>>> b3d378d6ac212a448fcebda2b8e553a839a379ec
-    case "Person6":
-      return <DemoPage />;
-    case "Person7":
-      return <DemoPage />;
-    case "Person8":
-      return <DemoPage />;
-    case "Person9":
-      return <DemoPage />;
-    case "Person10":
-      return <DemoPage />;
+
     default:
       return <div>Profile not found</div>;
   }
